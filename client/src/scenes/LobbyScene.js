e309b1eab9e442005e7ff4d006753da38840eede
Yuki <rg35300@gmail.com>
2026-07-17 14:43:07 +0200

    Chat Position Infinite Version 2

--- client/src/scenes/LobbyScene.js
+++ client/src/scenes/LobbyScene.js
@@ -459,7 +459,7 @@ export default class LobbyScene extends Phaser.Scene {
 
 
         input.style.top =
-        "310px";
+        "410px";
 
 
 
@@ -511,7 +511,7 @@ export default class LobbyScene extends Phaser.Scene {
 
 
         button.style.top =
-        "310px";
+        "410px";
 
 
 
